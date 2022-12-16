c826122c9c4c6aa32256e5c349694b72378d4307
Thachanon DK <47318864+thachanon27@users.noreply.github.com>
2022-12-16 14:13:11 +0700

    Update InvEnv25_16act.py

--- inv_env/envs/InvEnv25_16act.py
+++ inv_env/envs/InvEnv25_16act.py
@@ -854,6 +854,7 @@ class InvEnv4(gym.Env):
                             + sum_extra_penalty + sum_extra_penalty_2 + sum_extra_penalty_3
                             + (
                                        extra_p_on1_1 + extra_p_on1_2 + extra_p_on1_3 + extra_p_on2_1 + extra_p_on2_2 + extra_p_on2_3)) / 1000000)) / 100
+        print("reward",reward)
         
         #for Gelu activation fn
         #sum_extra_reward/1000000
