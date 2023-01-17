e0447be69dd2e71c91a8139b20dab7e1a7db5978
Thachanon DK <47318864+thachanon27@users.noreply.github.com>
2023-01-17 09:03:36 +0700

    Update InvEnv25_16act.py

--- inv_env/envs/InvEnv25_16act.py
+++ inv_env/envs/InvEnv25_16act.py
@@ -98,7 +98,7 @@ class InvEnv4(gym.Env):
         self.step_count = 0
         # state 14 dimension =onhand ,demand ,production status of machines
         self.state = np.array([
-             (self.on_hand1-0)/(12000-0), (self.on_hand2-0)/(10000-0), (self.on_hand3-0)/(9000-0),  # initial inventory  #ก่อนป้อนเข้า env ต้องทำเป็นค่า normalize แบบค่าอื่นก่อน
+             (np.random.randint(3500, 6500)-0)/(12000-0), (np.random.randint(2500, 4500)-0)/(10000-0), (np.random.randint(2000, 3500)-0)/(9000-0),  # initial inventory  #ก่อนป้อนเข้า env ต้องทำเป็นค่า normalize แบบค่าอื่นก่อน
             #np.random.randint(3500, 6500), np.random.randint(2500, 4500), np.random.randint(2000, 3500),
             # initial inventory แบบ random
             0, 0, 0,  # initial demand
