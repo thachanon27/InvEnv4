261d1d1f8c4c0a8df40fe7aa55e4bd909926c82e
Thachanon DK <47318864+thachanon27@users.noreply.github.com>
2023-05-02 12:34:43 +0700

    Update Inv_env_sen_set2.py

--- inv_env/envs/Inv_env_sen_set2.py
+++ inv_env/envs/Inv_env_sen_set2.py
@@ -141,9 +141,9 @@ class InvSen2(gym.Env):
         h2 = 29.86  # 34.53
         h3 = 47.77  # 48.66
         # Lost of good Will
-        k1 = 102000
-        k2 = 62000
-        k3 = 99200
+        k1 = 51000
+        k2 = 31000
+        k3 = 49600
         # Sell price
         p1 = 5100
         p2 = 3100
