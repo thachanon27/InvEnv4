9c01529da99b71c1e14db1b872e9b245c676c72c
Thachanon DK <47318864+thachanon27@users.noreply.github.com>
2023-09-27 12:55:49 +0700

    Update InvEnv27_A1_season_test_set2_60T_m2.py

--- inv_env/envs/InvEnv27_A1_season_test_set2_60T_m2.py
+++ inv_env/envs/InvEnv27_A1_season_test_set2_60T_m2.py
@@ -2164,6 +2164,6 @@ def test_production_table():
 
 ############################################################
 
-if __name__ == '__main__':
-    main()
-    #test_production_table()
+# if __name__ == '__main__':
+#     main()
+#     #test_production_table()
