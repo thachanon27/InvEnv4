3f1a4b437cd9fe5251139ffb1feb0db0fc4f9d41
Thachanon DK <47318864+thachanon27@users.noreply.github.com>
2024-02-08 14:11:40 +0700

    Update InvEnv26_A4_highseason_60T_add_action_noP.py

--- inv_env/envs/InvEnv26_A4_highseason_60T_add_action_noP.py
+++ inv_env/envs/InvEnv26_A4_highseason_60T_add_action_noP.py
@@ -25,6 +25,7 @@ from random import randint, choice
 # file 18  is demand set1  but file22 will demand set2 ต่างกันแค่นี้
 # print("new env @18-2-66")
 print_result = False
+print_demand_set = False
 
 
 ############################################################
@@ -1831,10 +1832,11 @@ class InvEnv5_60T_a4(gym.Env):
         done = bool(self.step_count >= 60)  # planning time frame period = 15
         self.overall_time_trained += 1
 
-        if self.step_count == 59:
-            print("demand_arr_inf in env at step 59 = ", demand_arr_inf[:6])  #ปริ้นแค่หกตัวแรก
-        if self.step_count == 60:
-            print("demand_arr_inf in env at step 60 = ", demand_arr_inf[:6])  #ปริ้นแค่หกตัวแรก
+        if print_demand_result == True:
+            if self.step_count == 59:
+                print("demand_arr_inf in env at step 59 = ", demand_arr_inf[:6])  #ปริ้นแค่หกตัวแรก
+            if self.step_count == 60:
+                print("demand_arr_inf in env at step 60 = ", demand_arr_inf[:6])  #ปริ้นแค่หกตัวแรก
 
         # if done == True:
         #   self.rn = np.random.random_integers(1, high=100000, size=None)
