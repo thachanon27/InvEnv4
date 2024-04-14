7661acba0def6ef3f23ce42f5b831f4bc45d3487
Thachanon DK <47318864+thachanon27@users.noreply.github.com>
2024-04-14 20:53:52 +0700

    Update InvEnv26_A1_randomV_60T_add_action2_noP.py

--- inv_env/envs/InvEnv26_A1_randomV_60T_add_action2_noP.py
+++ inv_env/envs/InvEnv26_A1_randomV_60T_add_action2_noP.py
@@ -470,11 +470,11 @@ class InvEnv5_60T_a1(gym.Env):
         if aaa == 1:
             demand_r1 = np.random.randint(3500, 4000)  # (2500, 4500) #avg + - 15%
             demand_r2 = np.random.randint(2500, 3300)  # 2000, 3500
-            demand_r3 = np.random.randint(700, 2400)  # 1000, 2500
+            demand_r3 = np.random.randint(1400, 2400)  # 1000, 2500
         if aaa == 2:
-            demand_r1 = np.random.randint(1800, 4200)  # (2500, 4500) #avg + - 15%
-            demand_r2 = np.random.randint(1500, 3600)  # 2000, 3500
-            demand_r3 = np.random.randint(700, 2800)  # 1000, 2500
+            demand_r1 = np.random.randint(2800, 3200)  # (2500, 4500) #avg + - 15%
+            demand_r2 = np.random.randint(1900, 2600)  # 2000, 3500
+            demand_r3 = np.random.randint(1100, 2100)  # 1000, 2500
 
         return index2, demand_r1, demand_r2, demand_r3, aaa
 
